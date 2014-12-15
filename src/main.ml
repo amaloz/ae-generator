73fa2a1da36b68e9fb918d1a82492f84fd0240b9
Alex Malozemoff <amalozemoff1@gmail.com>
2014-12-14 19:14:15 -0500

    refactor out common arguments

--- src/main.ml
+++ src/main.ml
@@ -13,6 +13,11 @@ let ocb = {
 
 let modes = "OCB"
 
+let spec_common =
+  let open Command.Spec in
+  empty
+  +> flag "-debug" (optional_with_default 0 int) ~doc:"N Set debug level to N (0 ≤ N ≤ 4)"
+
 let spec_check =
   let open Command.Spec in
   empty
@@ -23,8 +28,8 @@ let spec_check =
   +> flag "-check" no_arg ~doc:"Check if mode is secure"
   +> flag "-display" no_arg ~doc:"Display mode as a graph (needs 'dot' and 'feh')"
   +> flag "-eval" no_arg ~doc:"Evaluate the given mode"
-  +> flag "-file" (optional string) ~doc:"F Run against modes given in F"
-  +> flag "-debug" (optional_with_default 0 int) ~doc:"N Set debug level to N (0 ≤ N ≤ 4)"
+  +> flag "-file" (optional file) ~doc:"F Run against modes given in F"
+  ++ spec_common
 
 let run_check mode decode tag check display eval file debug () =
   Utils.debug_config debug;
@@ -80,55 +85,15 @@ let run_check mode decode tag check display eval file debug () =
   match file with
   | None -> run mode
   | Some file -> failwith "not implemented yet"
-  (* | fn -> *)
-  (*   let blocks = ref [] in *)
-  (*   let maxsize = ref 0 in *)
-  (*   (\* duplicate items table *\) *)
-  (*   let tbl = String.Table.create () ~size:1024 in *)
-  (*   let parse line = *)
-  (*     if line = "" then () *)
-  (*     else if String.contains ~pos:0 ~len:1 line '#' then () *)
-  (*     else *)
-  (*       let f c line = *)
-  (*         try String.index_exn line c with *)
-  (*         | Not_found -> failwith "Fatal: invalid format" *)
-  (*       in *)
-  (*       let a = f '(' line in *)
-  (*       let b = f ')' line in *)
-  (*       let block = String.sub line (a + 1) (b - 1) in *)
-  (*       let size = (String.count block (fun c -> c = ' ')) + 1 in *)
-  (*       if size > !maxsize then *)
-  (*         maxsize := size; *)
-  (*       try run !arg_init block with *)
-  (*       | Success -> *)
-  (*         if AeGeneration.exists *)
-  (*             ~keep_dups:(not !arg_remove_dups) *)
-  (*             tbl *)
-  (*             (to_insts !arg_init Init) *)
-  (*             (to_insts block Block) *)
-  (*         then *)
-  (*           () *)
-  (*         else *)
-  (*           let block = to_insts block Block in *)
-  (*           blocks := block :: !blocks *)
-  (*       | Failed -> () *)
-  (*   in *)
-  (*   let ic = In_channel.create fn in *)
-  (*   let stream = Stream.from (fun _ -> In_channel.input_line ic) in *)
-  (*   Stream.iter parse stream; *)
-  (*   In_channel.close ic; *)
-  (*   (\* print out relevant info *\) *)
-  (*   AeInst.print_modes !blocks !maxsize; *)
-  (*   printf "# found modes: %d\n" (List.length !blocks); *)
-  (*   for i = 1 to !maxsize do *)
-  (*     printf "# modes of size %d = %d\n%!" i (AeInst.count !blocks i) *)
-  (*   done *)
 
 let spec_synth =
   let open Command.Spec in
   empty
+  ++ spec_common
 
-let run_synth () = failwith "Not implemented yet"
+let run_synth debug () =
+  Utils.debug_config debug;
+  failwith "Not implemented yet"
 
 let check =
   Command.basic
