61100c2aa53f8f9e60b7ed6ebaf299084520a23a
Alex J. Malozemoff <amaloz@cs.umd.edu>
2015-05-31 21:16:50 -0400

    script for computing found modes, and updated found modes

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+for i in {12..16}
+do
+    echo "Size = $i"
+    time timeout 2h ./main.native synth -simple -print -size $i > $i.txt
+done
+
+cat 16.txt 15.txt 14.txt 13.txt 12.txt > found-modes.txt
