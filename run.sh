58fcde42d900be25d4acd8b0b7bd30e9e4497083
Alex J. Malozemoff <amaloz@cs.umd.edu>
2015-06-15 19:12:07 -0400

    run for 3 hours before timing out

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 for i in {12..16}
 do
     echo "Size = $i"
-    time timeout 2h ./main.native synth -simple -print -size $i > $i.txt
+    time timeout 3h ./main.native synth -simple -print -size $i > $i.txt
 done
 
 cat 16.txt 15.txt 14.txt 13.txt 12.txt > found-modes.txt
