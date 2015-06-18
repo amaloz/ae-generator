454603e57e579da4414fc14436f830595a3434eb
Alex J. Malozemoff <amaloz@cs.umd.edu>
2015-06-18 17:17:37 -0400

    only output to found-modes.txt non-duplicates

--- run.sh
+++ run.sh
@@ -6,4 +6,6 @@ do
     time timeout 3h ./main.native synth -simple -print -size $i > $i.txt
 done
 
-cat 16.txt 15.txt 14.txt 13.txt 12.txt > found-modes.txt
+cat 16.txt 15.txt 14.txt 13.txt 12.txt > tmp.txt
+./main.native check -simple -file tmp.txt -check > found-modes.txt
+rm tmp.txt
