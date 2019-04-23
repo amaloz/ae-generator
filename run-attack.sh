341cef06974db2256543617d6f40cee61af1c05e
Alex J. Malozemoff <amaloz@galois.com>
2019-04-23 16:44:00 -0700

    update scripts

--- run-attack.sh
+++ run-attack.sh
@@ -3,9 +3,9 @@
 for i in {12..15}
 do
     echo "Size = $i"
-    time timeout 6h ./main.native synth -decode -simple -print -size $i -attack > $i-attack.txt
+    time timeout 6h dune exec bin/main.exe -- synth -decode -simple -print -size $i -attack > $i-attack.txt
 done
 
 cat 15-attack.txt 14-attack.txt 13-attack.txt 12-attack.txt > tmp.txt
-./main.native check -simple -dec-file tmp.txt -attack > found-modes-attack.txt
+dune exec bin/main.exe -- check -simple -dec-file tmp.txt -attack > found-modes-attack.txt
 rm tmp.txt
